c99c1fa46703d0fe3995a881c40ee8e76401e99f
Wallace Copen <151352623+WallaceCopen@users.noreply.github.com>
2025-09-07 13:55:45 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import Navbar from "./components/navbar/Navbar"
 const App: React.FC = () => {
   return (
     <>
-      <Navbar />
-      <h1>Welcome to my Portfolio</h1>
+       <Navbar />
+      <div className='Home'>
+        <h1>Wallace Copen</h1>
+        <h2>Developer • Designer</h2>
+     </div>
     </>
   );
 };
