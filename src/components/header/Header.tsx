33cc98818116645a3c9323c4da5451785ad47ac0
Wallace Copen <wallacecopen18@gmail.com>
2025-09-08 19:23:22 -0700

    Added new component, contactIcons. Changed some configs for ts stuff. Added some blue accent hover effects.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,19 @@
 import { Paragraph, Title } from '../Text';
 import GridBackground from '../GridBackground';
 import styles from './Header.module.css';
+import Container from '../Container';
+import ContactIcons from './contact/ContactIcons';
+
+
 
 const Header: React.FC = () => (
     <div className={styles.header}>
         <GridBackground />
         <Title className={styles.title}>Wallace Copen</Title>
         <Paragraph className={styles.subtitle}>Developer • Designer</Paragraph>
+        <Container>
+            <ContactIcons />
+        </Container>
     </div>
 );
 
